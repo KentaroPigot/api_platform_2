af71aa48db7579b331a6ec715714c57b49eba6de
kentaroPigot <kentaropigot@gmail.com>
2025-01-22 02:11:04 +0100

    dependency update/debugged

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,7 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import { createApp } from "vue";
+import CheeseWhizApp from "./components/CheeseWhizApp.vue";
+import "bootstrap/dist/css/bootstrap.css";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+const app = createApp(CheeseWhizApp);
+
+app.mount("#cheese-app");
